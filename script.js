5b634ccfe61cd8d91f4e1a6b2e1b7b2608ed8631
Marcus Matthews <mlmatthews75@gmail.com>
2023-09-10 16:35:15 -0400

    added more changes to JS

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 // Use fetch for getting the weather website (API)
-const citySearch = $("#search-box")
-const searchButton = $("#search-button")
-const searchHistory = $("#search-history")
-const currentWeather = $("#current-weather")
-const forecastWeather = $("#days-forecast")
+const searchBox = document.querySelector(".search-box");
+const searchButton = document.querySelector(".search-button");
+const searchHistory = document.querySelector(".search-history");
+const currentWeather = document.querySelector(".current-weather");
+const forecastWeather = document.querySelector(".days-forecast");
 
-function fetchCity () {
-    const citySearch
+function fetchCity() {
+    const cityName = searchBox.value.trim();
+    if (!cityName) return;
+    console.log(cityName);
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=b647a61a800d5facb7c740cf2bda53b0')
+    console.log(fetchCity);
+
+    fetch(cityCoordinates)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
 }
+
+$(searchButton).on('click', function () {
+    fetchCity(searchBox.val());
+});
