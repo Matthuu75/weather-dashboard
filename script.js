eeb4266f5628f49750e05818bc87995863807053
Marcus Matthews <mlmatthews75@gmail.com>
2023-09-10 17:41:50 -0400

    added API call to coordinates and city

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 // Use fetch for getting the weather website (API)
-const searchBox = document.querySelector(".search-box");
-const searchButton = document.querySelector(".search-button");
-const searchHistory = document.querySelector(".search-history");
-const currentWeather = document.querySelector(".current-weather");
-const forecastWeather = document.querySelector(".days-forecast");
+const searchBox = document.querySelector("#search-box");
+const searchButton = document.querySelector("#search-button");
+const searchHistory = document.querySelector("#search-history");
+const currentWeather = document.querySelector("#current-weather");
+const forecastWeather = document.querySelector("#days-forecast");
 
-function fetchCity() {
-    const cityName = searchBox.value.trim();
+const weatherInfo = (cityName, lat, lon) => {
+    const cityCoordinates = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b647a61a800d5facb7c740cf2bda53b0`
+    fetch(cityCoordinates)
+        .then(response => response.json()).then(cityData => {
+        console.log(cityData)
+    })
+}
+
+function fetchWeather() {
+    const cityName = searchBox.value;
     if (!cityName) return;
-    console.log(cityName);
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=b647a61a800d5facb7c740cf2bda53b0')
-    console.log(fetchCity);
+    const cityCoordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=b647a61a800d5facb7c740cf2bda53b0`
 
     fetch(cityCoordinates)
         .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
+        .then(cityData => {
+            const { name, lat, lon} = cityData[0];
+            weatherInfo(name, lat, lon);
+        });
 }
 
-$(searchButton).on('click', function () {
-    fetchCity(searchBox.val());
-});
+searchButton.addEventListener("click", fetchWeather);
+
+// $(searchButton).on('click', function () {
+//     fetchCity(searchBox.val());
+// });
