1997a2a12010a60c979ec2a1a94c67aec583a639
Marcus Matthews <mlmatthews75@gmail.com>
2023-09-10 15:52:08 -0400

    added HTMl page changes. Started new JS

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 // Use fetch for getting the weather website (API)
+const citySearch = $("#search-box")
+const searchButton = $("#search-button")
+const searchHistory = $("#search-history")
+const currentWeather = $("#current-weather")
+const forecastWeather = $("#days-forecast")
 
-const searchBoxEl = $('#search-box');
-const searchButtonEl = $('#search-button');
-const searchLogEl = $('#search-log');
-const weatherEl = $('#weather');
-
-function fetchWeather (city) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b647a61a800d5facb7c740cf2bda53b0')
-    .then(response => {
-        return response.json();
-    })
-    .then(response => {
-        lon = (response[0].lon);
-        lat = (response[0].lat);
-
-        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b647a61a800d5facb7c740cf2bda53b0')
-    })
+function fetchCity () {
+    const citySearch
 }
